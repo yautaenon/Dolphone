bd2c91a8f8ed2a10de42011429551416dffc1ea0
yautaenon <53148052+yautaenon@users.noreply.github.com>
2023-05-30 00:08:42 +0900

    Commit

--- Dolpthone_inject_Beta.js
+++ Dolpthone_inject_Beta.js
@@ -1,4 +1,4 @@
-window_5209hbf298hew9jfiowg_Dolpthone_version = "1.1.0-";
+window_5209hbf298hew9jfiowg_Dolpthone_version = "1.1.0";
 var window_b9012ye1by98ryn98dy1_Dolpthone_BranchID;
 var dolphoneJS, dolphoneCSS, dolphoneIndicator;
 $.getJSON(
@@ -9,7 +9,7 @@ $.getJSON(
     dolphoneCSS = data.css;
     dolphoneIndicator = data.indicator;
     window_5209hbf298hew9jfiowg_Dolpthone_version =
-      window_5209hbf298hew9jfiowg_Dolpthone_version + data.version;
+      window_5209hbf298hew9jfiowg_Dolpthone_version + "-" + data.version;
     if (window_b9012ye1by98ryn98dy1_Dolpthone_BranchID === undefined)
       window_b9012ye1by98ryn98dy1_Dolpthone_BranchID = "1114918";
     window_ony98gvayn9fj8hg98ha_Dolpthone_versionType = "_Beta";
