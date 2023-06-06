d156876eb80b69440b8b6610fc17e9aa6fb27004
yautaenon <53148052+yautaenon@users.noreply.github.com>
2023-06-06 20:25:54 +0900

    Commit

--- Dolpthone_inject_Beta.js
+++ Dolpthone_inject_Beta.js
@@ -125,7 +125,7 @@ function videochecker() {
       }, 200);
       libalert("VideoDurationChecker", "process started.");
     }
-  }, 20);
+  }, 100);
 }
 function done() {
   console.log("DONE!");
